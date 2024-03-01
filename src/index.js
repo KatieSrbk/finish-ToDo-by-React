a7698e69a91c61b000c62d74326f33e7acace037
KatieSrbk <miss.kate.sh@gmail.com>
2024-03-01 20:55:14 +0300

    install chakra-ui

--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {
+  ChakraBaseProvider,
+  ChakraProvider,
+  extendBaseTheme,
+} from '@chakra-ui/react';
+
+const theme = extendBaseTheme({});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <ChakraBaseProvider theme={theme}>
+        <App />
+      </ChakraBaseProvider>
+    </ChakraProvider>
   </React.StrictMode>
 );
-
